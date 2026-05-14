9771232282e3a4c8dc3f47bd552f295a3e8aa70f
Nelson Rodrigues <nelsonrodrigues87@outlook.com>
2026-05-14 19:04:53 +0100

    Set default path to the current working directory

--- cmd/generate.go
+++ cmd/generate.go
@@ -7,16 +7,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var outputPath string
+
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:     "generate",
 	Aliases: []string{"g"},
 	Args:    cobra.MinimumNArgs(1),
 	Short:   "Generates a database migration for a new entity table",
-	Run:     generateFn,
+	Run:     runGenerateCmd,
 }
 
-func generateFn(cmd *cobra.Command, args []string) {
+func runGenerateCmd(cmd *cobra.Command, args []string) {
 	name, err := lib.NormalizeName(args[0])
 	if err != nil {
 		cmd.PrintErrf("%s\n", err)
@@ -35,10 +37,10 @@ func generateFn(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
-	dirPath := "db/migrate"
-	err = lib.CreateFile(dirPath, filename, sql)
+	outputPath = "."
+	err = lib.CreateFile(outputPath, filename, sql)
 	if err != nil {
-		cmd.PrintErrf("Error creating sql file: %s\n", filename)
+		cmd.PrintErrf("Error creating schema file: %s\n", filename)
 		cmd.PrintErrf("%s\n", err)
 		os.Exit(1)
 	}
